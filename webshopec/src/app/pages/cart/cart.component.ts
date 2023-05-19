3bd781a4983c7c4c7e57fe5dd9ae9ecab797fa34
victoroff <mrbebz@gmail.com>
2023-05-19 10:57:01 +0300

    add stripejs version 1.35.0

--- webshopec/src/app/pages/cart/cart.component.ts
+++ webshopec/src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js'
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -8,7 +10,8 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CartComponent implements OnInit {
 
-  constructor(private cartService: CartService) { }
+
+  constructor(private cartService: CartService, private httpClient: HttpClient) { }
 
   cart: Cart = {items: [{
     product: 'https://via.placeholder.com/150',
@@ -62,6 +65,17 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem): void {
    this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.httpClient.post('http://localhost:4200/checkout',{
+      items: this.cart.items
+    }).subscribe(async (res: any) => {
+      let stripe = await loadStripe('pk_test_51N8jGYFb9v1C0r9kF7UidazNGQZV7E6PZzjmUFMQiyZdg9Z8kccfamkFJJgbW6xTPw87V640ZbxzEyaaZ2ioHuBi00wg1A9unV');
+        stripe?.redirectToCheckout({
+          sessionId:res.id
+        })
+    });
   }
     
 }
